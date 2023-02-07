5fc60f1d2c6a628deb7482f1e2769abdf00f28bc
saisrisudha <120887265+saisrisudha@users.noreply.github.com>
2023-02-07 22:05:19 +0530

    07-02-23 java practice

---
+++ greatestof2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class great{
+             public static void main(String args[])
+           {
+              Scanner sc=new Scanner(System.in);
+              System.out.println("enter number");
+              int num1=sc.nextInt();
+             System.out.println("enter number two");
+            int num2=sc.nextInt(); 
+            if(num1>num2)
+           {
+           System.out.println("is the greatest"+num1);
+            }
+           else{
+           System.out.println("is the greatest"+num2);
+           }
+      }
+}
+
