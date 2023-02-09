47dad43e37afa827cd6b018ef5886216d3c50758
saisrisudha <120887265+saisrisudha@users.noreply.github.com>
2023-02-09 21:04:56 +0530

    09-02-23 java practice

---
+++ even.java
@@ -0,0 +1,12 @@
+class even{
+           public static void main(String args[])
+           {
+     for(int a=1;a<=100;a++)
+  {
+    if(a%2==0)
+     {
+          System.out.println(a);
+              }
+       }
+   }
+}
---
+++ one.java
@@ -0,0 +1,9 @@
+class one{
+ public static void main (String args[])
+{
+for(int a=1;a<=100;a++)
+{
+System.out.println(a);
+}
+}
+}
